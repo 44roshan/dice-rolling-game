2b3da466affd88b54f2ea00a961ca56ded526ca0
roshan <443roshanjp@gmail.com>
2025-08-14 01:08:15 +0900

    small update

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+# import random
+# print("Hello! there Welcome")
+# while True:
+#     random_number = random.randint(1, 6)
+#     initial = input("Roll the dice? Y/N:").lower()
+#     if initial == "y":
+#         print(random_number)
+#     elif initial == "n":
+#         print("you press no")
+#         break
+#     else:
+#         print("Invalid command")
 import random
-print("Hello! there Welcome")
 while True:
-    random_number = random.randint(1, 6)
-    initial = input("Roll the dice? Y/N:").lower()
-    if initial == "y":
-        print(random_number)
-    elif initial == "n":
-        print("you press no")
+    test = input("Hello Everyone, do you want to roll the dice? Y/N").lower()
+    if test == "y":
+        print(
+            f"Your random number is ({random.randint(1, 6)}, {random.randint(1, 6)})")
+    elif test == "n":
+        print("you choose not to roll")
         break
     else:
-        print("Invalid command")
+        print("Invalid input, Please enter Y or N")
